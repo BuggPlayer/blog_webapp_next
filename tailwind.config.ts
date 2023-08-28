c08a356a34c36111e5430b1b5354b600b95fa330
Hifzur Rehman Alauddin Khan <hifzur.khan@magicsw.com>
2023-08-29 01:24:46 +0530

    color combinations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,25 @@ import type { Config } from "tailwindcss";
 // #0E353F
 // #767676
 
+// #20727F
+// #0E353F
+// #767676
+
+/* 
+ color combination 1
+Primary Color: Dark Blue (#001F3F) --> navabr and footer // behind the text bg
+Secondary Color: Light Gray (#D3D3D3)  -> overall background of the blog // text body
+Accent Color: Electric Blue (#0074D9) --> heading and sub-heading // call to action BUTTONs // highlight important information, such as quotes, key points, or links
+Highlight Color: Green (#2ECC40) -->pply the highlight color to secondary elements like icons, dividers, or decorative elements to add visual interest.
+
+
+color combination 2
+Primary Color: Slate Blue (#6A5ACD) 
+Secondary Color: Light Gray (#EAEAEA)
+Accent Color: Orange (#FFA500)
+Highlight Color: Teal (#008080)
+
+*/
 
 const config: Config = {
   content: [
