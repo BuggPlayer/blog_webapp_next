bf3e43436c4ef8c78955115e2d439656271ce45b
Hifzur Rehman Alauddin Khan <hifzur.khan@magicsw.com>
2023-09-03 21:19:54 +0530

    build

--- tesrcode.ts
+++
@@ -1,8 +0,0 @@
-const getSingleCategory = async () => {
-    const res = await fetch(
-      `${BaseURl}api/category/singlecategory?${params.category}`
-    );
-    // console.log(res.json());
-
-    return await res.json();
-  };
