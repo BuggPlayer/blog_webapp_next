f8bb24c5344b4edf10c80ea9cda830f00ecfe760
Hifzur Rehman Alauddin Khan <hifzur.khan@magicsw.com>
2023-09-05 11:26:47 +0530

    size change

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <div className="bg-gray-200 h-screen w-full  flex items-center justify-center">
-      <Image src="/loading-state.gif" height={40} width={40} alt="no" />;
+      <Image src="/loading-state.gif" height={60} width={60} alt="no" />;
     </div>
   );
 }
