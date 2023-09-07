ed86c06201bf4945aa28c85fe0a57b49bf330ed8
Hifzur Rehman Alauddin Khan <hifzur.khan@magicsw.com>
2023-09-07 12:09:15 +0530

    .

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -51,10 +51,41 @@ const BlogCard = async ({ article }: any) => {
           {article?.content?.length > 250 ? "..." : ""}
         </div> */}
 
-        <div className=" flex gap-3 mt-4">
+        <div className=" flex justify-between text-gray-600 gap-3 mt-4">
           <p className="border  inline-block text-sm px-2 bg-backgroundColor text-subHeadingTextColor rounded-xl ">
             {article.category}
           </p>
+          <div className=" flex gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+              />
+            </svg>
+          </div>
         </div>
       </div>
 
